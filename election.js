2238ae1dc469a9178397a0e9b237f8bd7bd32f7e
Valerie Boem <vboem@gmail.com>
2017-02-18 18:21:49 -0500

    Added the candidates to main page with list and created Ajax request

--- election.js
+++ election.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
-  // Imagination!
-
+  $.ajax({
+    method: 'GET',
+    url: 'https://bb-election-api.herokuapp.com/',
+    dataType:'JSON',
+  }).done(function(data){
+    console.log(data)
+    for(var i=0; i < data.candidates.length; i++) {
+     $('#votes').append('<li> Name: ' + data.candidates[i].name + '</li>')
+     $('#votes').append('<li> Votes: ' + data.candidates[i].votes + '</li><br>')
+    };
+  });
 });
